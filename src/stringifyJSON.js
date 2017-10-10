6c2c6c238ac8d84760a717d793e17679db3e8854
sukhpaul <sukhpaul.nijjar@gmail.com>
2017-10-09 18:12:03 -0700

    pass all tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null){
+    return 'null';
+} 
+  if (typeof obj === 'string') {
+    return '"' + obj.toString() + '"';
+}
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+} if (Array.isArray(obj)){
+    if (obj.length > 0){
+      return '[' + _.map(obj,function(element){
+        return stringifyJSON(element);
+    }) + ']';
+    }
+    return '[]';
+} if (typeof obj === 'object'){
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }   
+   // var newObj = {};
+   // for (var key in obj){
+   //    debugger;
+   //    var stringifyKey = stringifyJSON(key);
+   //    var stringifyValue = stringifyJSON(obj[key]);
+   //    newObj[stringifyKey] = stringifyValue; 
+   // }
+   //  return '{' + newObj + '}';
+  var stringed = [];
+  for (var key in obj){
+    if (typeof obj[key] === 'function' || obj[key] === undefined) {
+      return '{}';
+    }
+      var stringifyKey = stringifyJSON(key);
+      var stringifyValue = stringifyJSON(obj[key]);
+      stringed.push(stringifyKey + ':' + stringifyValue);
+    }
+    return '{' + stringed + '}';
+} 
 };
